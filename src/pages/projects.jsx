f9fda9c385d728aefe6d80f2ae049bd78de171da
Ahmed Raed <ahmedraed101@yahoo.com>
2024-08-23 20:44:07 +0300

    fixing-some-errors

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,20 +1,20 @@
 export default function Projects() {
   return (
     <>
-      <div class="bg-white font-[sans-serif] py-28 px-6 sm:px-10 text-new-font">
-        <div class="max-w-7xl mx-auto">
-          <div class="flex flex-col text-start">
-            <h2 class="text-3xl font-extrabold text-[#333] inline-block relative mb-2">
+      <div className="bg-white font-[sans-serif] py-28 px-6 sm:px-10 text-new-font">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex flex-col text-start">
+            <h2 className="text-3xl font-extrabold text-[#333] inline-block relative mb-2">
               Projects ✨
             </h2>
-            <h2 class="text-xl font-bold text-[#333] inline-block relative">
+            <h2 className="text-xl font-bold text-[#333] inline-block relative">
               Explore a selection of my latest projects.
             </h2>
           </div>
-          <div class="flex flex-row gap-1 w-fit mt-6">
+          <div className="flex flex-row gap-1 w-fit mt-6">
             <select
               id="countries"
-              class="basis-1/2 p-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block"
+              className="basis-1/2 p-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block"
             >
               <option selected>All Categories</option>
               <option value="US">Web</option>
@@ -35,19 +35,21 @@ export default function Projects() {
               <option value="DE">Python</option>
             </select> */}
           </div>
-          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16 max-md:max-w-lg mx-auto">
-            <div class="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16 max-md:max-w-lg mx-auto">
+            <div className="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
               <img
                 src="https://readymadeui.com/Imagination.webp"
                 alt="Blog Post 1"
-                class="w-full h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
-              <div class="pt-4">
-                {/* <span class="text-sm block text-gray-400 mb-2">
+              <div className="pt-4">
+                {/* <span className="text-sm block text-gray-400 mb-2">
                   10 FEB 2023
                 </span> */}
-                <h3 class="text-3xl font-bold text-[#333] mb-1">Project 1</h3>
-                <p class="text-gray-700 text-sm font-bold">
+                <h3 className="text-3xl font-bold text-[#333] mb-1">
+                  Project 1
+                </h3>
+                <p className="text-gray-700 text-sm font-bold">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                   accumsan, nunc et tempus blandit, metus mi consectetur felis
                   turpis vitae ligula.
@@ -62,18 +64,20 @@ export default function Projects() {
                 </button>
               </div>
             </div>
-            <div class="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+            <div className="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
               <img
                 src="https://readymadeui.com/Imagination.webp"
                 alt="Blog Post 1"
-                class="w-full h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
-              <div class="pt-4">
-                {/* <span class="text-sm block text-gray-400 mb-2">
+              <div className="pt-4">
+                {/* <span className="text-sm block text-gray-400 mb-2">
                   10 FEB 2023
                 </span> */}
-                <h3 class="text-3xl font-bold text-[#333] mb-1">Project 2</h3>
-                <p class="text-gray-700 text-sm font-bold">
+                <h3 className="text-3xl font-bold text-[#333] mb-1">
+                  Project 2
+                </h3>
+                <p className="text-gray-700 text-sm font-bold">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                   accumsan, nunc et tempus blandit, metus mi consectetur felis
                   turpis vitae ligula.
@@ -88,18 +92,20 @@ export default function Projects() {
                 </button>
               </div>
             </div>
-            <div class="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+            <div className="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
               <img
                 src="https://readymadeui.com/Imagination.webp"
                 alt="Blog Post 1"
-                class="w-full h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
-              <div class="pt-4">
-                {/* <span class="text-sm block text-gray-400 mb-2">
+              <div className="pt-4">
+                {/* <span className="text-sm block text-gray-400 mb-2">
                   10 FEB 2023
                 </span> */}
-                <h3 class="text-3xl font-bold text-[#333] mb-1">Project 3</h3>
-                <p class="text-gray-700 text-sm font-bold">
+                <h3 className="text-3xl font-bold text-[#333] mb-1">
+                  Project 3
+                </h3>
+                <p className="text-gray-700 text-sm font-bold">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                   accumsan, nunc et tempus blandit, metus mi consectetur felis
                   turpis vitae ligula.
@@ -114,18 +120,20 @@ export default function Projects() {
                 </button>
               </div>
             </div>
-            <div class="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+            <div className="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
               <img
                 src="https://readymadeui.com/Imagination.webp"
                 alt="Blog Post 1"
-                class="w-full h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
-              <div class="pt-4">
-                {/* <span class="text-sm block text-gray-400 mb-2">
+              <div className="pt-4">
+                {/* <span className="text-sm block text-gray-400 mb-2">
                   10 FEB 2023
                 </span> */}
-                <h3 class="text-3xl font-bold text-[#333] mb-1">Project 4</h3>
-                <p class="text-gray-700 text-sm font-bold">
+                <h3 className="text-3xl font-bold text-[#333] mb-1">
+                  Project 4
+                </h3>
+                <p className="text-gray-700 text-sm font-bold">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                   accumsan, nunc et tempus blandit, metus mi consectetur felis
                   turpis vitae ligula.
@@ -140,18 +148,20 @@ export default function Projects() {
                 </button>
               </div>
             </div>
-            <div class="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+            <div className="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
               <img
                 src="https://readymadeui.com/Imagination.webp"
                 alt="Blog Post 1"
-                class="w-full h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
-              <div class="pt-4">
-                {/* <span class="text-sm block text-gray-400 mb-2">
+              <div className="pt-4">
+                {/* <span className="text-sm block text-gray-400 mb-2">
                   10 FEB 2023
                 </span> */}
-                <h3 class="text-3xl font-bold text-[#333] mb-1">Project 5</h3>
-                <p class="text-gray-700 text-sm font-bold">
+                <h3 className="text-3xl font-bold text-[#333] mb-1">
+                  Project 5
+                </h3>
+                <p className="text-gray-700 text-sm font-bold">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                   accumsan, nunc et tempus blandit, metus mi consectetur felis
                   turpis vitae ligula.
@@ -166,18 +176,20 @@ export default function Projects() {
                 </button>
               </div>
             </div>
-            <div class="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+            <div className="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
               <img
                 src="https://readymadeui.com/Imagination.webp"
                 alt="Blog Post 1"
-                class="w-full h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
-              <div class="pt-4">
-                {/* <span class="text-sm block text-gray-400 mb-2">
+              <div className="pt-4">
+                {/* <span className="text-sm block text-gray-400 mb-2">
                   10 FEB 2023
                 </span> */}
-                <h3 class="text-3xl font-bold text-[#333] mb-1">Project 6</h3>
-                <p class="text-gray-700 text-sm font-bold">
+                <h3 className="text-3xl font-bold text-[#333] mb-1">
+                  Project 6
+                </h3>
+                <p className="text-gray-700 text-sm font-bold">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                   accumsan, nunc et tempus blandit, metus mi consectetur felis
                   turpis vitae ligula.
@@ -192,18 +204,20 @@ export default function Projects() {
                 </button>
               </div>
             </div>
-            <div class="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
+            <div className="bg-white h-fit p-4 cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
               <img
                 src="https://readymadeui.com/Imagination.webp"
                 alt="Blog Post 1"
-                class="w-full h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
-              <div class="pt-4">
-                {/* <span class="text-sm block text-gray-400 mb-2">
+              <div className="pt-4">
+                {/* <span className="text-sm block text-gray-400 mb-2">
                   10 FEB 2023
                 </span> */}
-                <h3 class="text-3xl font-bold text-[#333] mb-1">Project 7</h3>
-                <p class="text-gray-700 text-sm font-bold">
+                <h3 className="text-3xl font-bold text-[#333] mb-1">
+                  Project 7
+                </h3>
+                <p className="text-gray-700 text-sm font-bold">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                   accumsan, nunc et tempus blandit, metus mi consectetur felis
                   turpis vitae ligula.
